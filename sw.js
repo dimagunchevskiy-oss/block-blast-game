b955b3188a2aa76f44bbbac1404a33b0060ebc0b
Dm1tryDm1try <dimagunchevskiy@gmail.com>
2026-03-25 13:42:44 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Блок Бласт — Service Worker
 // Версию меняй при каждом обновлении игры
-const CACHE = 'blokblast-v2';
+const CACHE = 'blokblast-v3';
 const ASSETS = [
   './',
   './index.html',
